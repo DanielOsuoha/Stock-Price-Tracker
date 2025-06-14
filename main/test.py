fffe82fe083450329cc30b6e3d0db19c2adedb06
Daniel Osuoha <122853068+DanielOsuoha@users.noreply.github.com>
2025-06-14 16:00:19 -0700

    Removed comments from test.py

--- main/test.py
+++ main/test.py
@@ -2,16 +2,13 @@ import pandas as pd
 import yfinance as yf
 import matplotlib.pyplot as plt
 
-# Set the API 
+
 crypto_currency = 'BTC'
 against_currency = 'USD'
-# Set the API parameters
-date_today = "2020-01-01" # period start date
-date_start = "2010-01-01" # period end date
-symbol = "" # asset symbol - For more symbols check yahoo.finance.com
+date_today = "2020-01-01"
+date_start = "2010-01-01" 
+symbol = "" 
 
-
-# Send the request to the yahoo finance api endpoint
 df = yf.download(f'{crypto_currency}-{against_currency}', date_start, date_today)
 print(df)
 
