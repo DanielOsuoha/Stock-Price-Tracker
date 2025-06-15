409121d963f21b9fd7b08498efc58a9f6c755081
Daniel Osuoha <122853068+DanielOsuoha@users.noreply.github.com>
2025-06-14 18:56:39 -0700

    made some changes

--- main/app.py
+++ main/app.py
@@ -9,17 +9,18 @@ app = Flask(__name__, template_folder="templates")
 def index():
     return render_template("index.html")
 
-# @app.route("/get_stock_data", methods=["POST"])
-# def get_stock_data():
-#     ticker = request.get_json()["ticker"]
-#     data = yf.Ticker(ticker).history(period="1y")
-#     prediction = get_prediction(ticker, 'USD')
+@app.route("/get_stock_data", methods=["POST"])
+def get_stock_data():
+    ticker = request.get_json()["ticker"]
+    data = yf.Ticker(ticker).history(period="1y")
+    prediction = get_prediction(ticker, 'USD')
     
-#     return jsonify({
-#         "currentPrice": data.iloc[-].Close,
-#         "openPrice": data.iloc[-1].Open,
-#         "predictedPrice": prediction
-#     })
+    return jsonify({
+        "currentPrice": data.iloc[-1].Close,
+        "openPrice": data.iloc[-1].Open,
+        "predictedPrice": 70.5
+        # "predictedPrice": prediction 
+    })
     
     
 if __name__ == '__main__':
--- main/static/script.js
+++ main/static/script.js
@@ -1,4 +1,4 @@
-var tickers = JSON.parse(localStorage.getItem("tickers")) || ["AAPL", "MSFT", "GOOGL", "AMZN", "TSLA"];
+var tickers = JSON.parse(localStorage.getItem("tickers")) || ["AAPL", "GOOGL", "AMZN", "MSFT", "TSLA"];
 var lastPrices = {};
 var counter = 10;
 
@@ -16,21 +16,25 @@ function startUpdateCycle(){
 }
 
 $(document).ready(function(){
+    console.log(tickers)
     tickers.forEach(function(ticker){
         addTickerTOGrid(ticker);
     });
-    updatePrices();
+    // updatePrices();
 
     $("#add-ticker-form").submit(function(event) {
         event.preventDefault();
         var ticker = $("#new-ticker").val().toUpperCase();
         if (!tickers.includes(ticker)) {
+            // We need to validate that this is an actual stock ticker
+            // I will have to implement a backend endpoint to validate the ticker
+            // For now, we will just add it to the list
             tickers.push(ticker);
             localStorage.setItem("tickers", JSON.stringify(tickers));
             addTickerTOGrid(ticker);
         }
         $("#new-ticker").val("");
-        updatePrices();
+        // updatePrices();
     });
 
     $("#tickers-grid").on("click", ".remove-btn", function(){
@@ -45,9 +49,9 @@ $(document).ready(function(){
 });
 
 function addTickerTOGrid(ticker){
-    var tickerDiv = $(`<div class="stock-box" id="${ticker}"></div>`);
-    tickerDiv.append(`<h3>${ticker}</h3>`);
-    tickerDiv.append(`<p id="${ticker}-price"></p> <p id="${ticker}-pct"></p>`);
+    var tickerDiv = $(`<div class="stock-box" id="${ticker} name"></div>`);
+    tickerDiv.append(`<h3>${ticker} man</h3>`);
+    tickerDiv.append(`<p id="${ticker}-price">val</p> <p id="${ticker}-pct"></p>`);
     tickerDiv.append(`<button class="remove-btn btn btn-outline-success" data-ticker="${ticker}">Remove</button>`);
     // tickerDiv.append(`<div class="ticker-price"></div>`);
     $("#tickers-grid").append(tickerDiv);
@@ -64,14 +68,10 @@ function updatePrices() {
             success: function(data) {
                 var changePercent = ((data.currentPrice - data.openPrice)/ data.openPrice) * 100;
                 var colorClass;
-                if (changePercent <= -2){
-                    colorClass = 'dark-red';
-                }else if (changePercent < 0){
+                if (changePercent < 0){
                     colorClass ='red';
                 }else if (changePercent == 0){
                     colorClass = 'gray';
-                }else if (changePercent <= 2){
-                    colorClass = 'green';
                 }else{
                     colorClass = 'dark-green';
                 }
